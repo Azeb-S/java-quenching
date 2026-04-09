e517bac7d8ba9c87a188fc14740b7719379c4303
Azeb Tesfay <azbseb@gmail.com>
2026-04-08 18:06:54 -0700

    ShorterestWord

--- src/Practice.java
+++ src/Practice.java
@@ -37,7 +37,29 @@ public class Practice {
      * @throws NullPointerException     if words is null
      */
     public static String shortestWord(Set<String> words) {
-        return null;
+        if (words == null) {
+            throw new NullPointerException();
+        }
+        if (words.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        String shortest = null;
+        for (String word : words) {
+            shortest = word;
+            break;
+        }
+        for (String word : words) {
+            if (word.length() < shortest.length()) {
+                shortest = word;
+
+            } else if (word.length() == shortest.length()) {
+                if (word.compareTo(shortest) < 0) {
+                    shortest = word;
+                }
+
+            }
+        }
+        return shortest;
     }
 
     /**
@@ -50,6 +72,7 @@ public class Practice {
      * @throws NullPointerException if ages is null
      */
     public static Set<String> adults(Map<String, Integer> ages) {
+
         return null;
     }
 
